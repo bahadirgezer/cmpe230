0d6e16adedc83de3fd718f7f1b9dd50bfe62e285
Bahadır Gezer <bahadirgezerbg@gmail.com>
2022-03-26 21:40:57 +0300

    Seperated string helper functions to a new file

---
+++ string_functions.h
@@ -0,0 +1,7 @@
+int isAlphaNumeric(char ch);
+int isAlphaNumericLiteral(char ch[]);
+int isLetter(char ch);
+int isLetterLiteral(char ch[]);
+int isNumber(char ch);
+int isNumberLiteral(char ch[]);
+int isParanthesis(char ch);
