31100e5309dbd954ab42603f761132a09b4d6313
Bahadır Gezer <bahadirgezerbg@gmail.com>
2022-03-27 19:08:16 +0300

    Tokenizer function fully working

--- main.c
+++ main.c
@@ -19,6 +19,8 @@ struct Token {
     @param line a single line in the .mat file
 */
 void tokenizer(char line[]) {
+    char append = ' '; // needs an not important character at the end to read the last token.
+    strncat(line, &append, 1);
     char *token_start = line;
     int line_len = strlen(line);
     int token_type = 0; //0: loopbegin, whitespace; 1: specialcharacter 2: alphanumeric
@@ -28,111 +30,77 @@ void tokenizer(char line[]) {
     int index = 0;
     while (index < line_len) {
         char current = line[index];
-        printf("%d\t", token_type);
-        //printf("%c\t", line[index]);
-        //printf("%d\t", token_type);
-        //printf("%d\n", token_len);
-        
+
         if (isWhitespace(current) == 1) {
-            if (token_type == 0 || token_type == 1) {
+            if (token_type == 0) {
+                token_len = 0;
                 index++;
                 token_start = line + index;
-                token_type = 0;
+            } else if (token_type == 1) {
+                extract_token(token_start, token_len);
                 token_len = 0;
-            } else {
-                printf("in1\n");
-                extract_token(token_start, token_len+1);
                 index++;
                 token_start = line + index;
-                token_type = 0;
+            } else if (token_type == 2) {
+                extract_token(token_start, token_len);
                 token_len = 0;
-            }
+                index++;
+                token_start = line + index;
+            }   
+            token_type = 0;     
             continue;
         }
 
-        // if (isWhitespace(current) == 1) {
-        //     if (token_type != 0) {
-        //         extract_token(token_start, token_len);
-        //         token_start = token_start + token_len + 1;
-        //         token_type = 0;
-        //         token_len = 0;
-        //     } else {
-        //         token_start = token_start + 1;
-        //         token_len = 0;
-        //     }
-        //     index++;
-        //     continue;
-        // }
-        
         if (isSpecialCharacter(current) == 1) {
-            if (token_type != 1 || token_type != 0) {
-                printf("in2\n");
-
-                extract_token(token_start, token_len+1);
-                extract_token(line + index, 1);
-                index++;
+            if (token_type == 0) {
+                token_len = 1;
                 token_start = line + index;
-                token_type = 1;
-                token_len = 0;
-            } else {
                 index++;
+            } else if (token_type == 1) {
+                extract_token(token_start, token_len);
+                token_len = 1;
                 token_start = line + index;
-                printf("in3\n");
-
-                extract_token(token_start, 1);
-                token_type = 1;
-                token_len = 0;
+                index++;
+            } else if (token_type == 2) {
+                extract_token(token_start, token_len);
+                token_len = 1;
+                token_start = line + index;
+                index++;
             }
-
+            token_type = 1;
             continue;
         }
-
-        // if (isSpecialCharacter(current) == 1) {
-        //     if (token_type != 0) {
-        //         extract_token(token_start, token_len);
-        //         token_start = token_start + token_len;
-        //         token_len = 0;
-        //         token_type = 1;
-        //     } else {
-        //         token_len = 1;
-        //         extract_token(token_start, token_len);
-        //         token_len = 0;
-        //         token_type = 1;
-        //     }
-        //     index++;
-        //     continue;
-        // }
 
         if (isAlphaNumeric(current) == 1) {
-            if (token_type == 2) {
+            if (token_type == 0) {
+                token_len = 1;
+                token_start = line + index;
+                index++;
+            } else if (token_type == 1) {
+                extract_token(token_start, token_len);
+                token_len = 1;
+                token_start = line + index;
+                index++;
+            } else if (token_type == 2) {
                 token_len++;
                 index++;
-            } else {
-                token_start = line + index;
-                token_type = 2;
-                token_len = 0;
-                index++;
             }
+            token_type = 2;
             continue;
         }
-  
 
-        printf("error on line _");
+        printf("This line has an error\n");
         break;
     }
 }
 
 void extract_token(int *token_start, int token_len) {
     Token token;
-    // printf("%p\t", token_start);
-    // printf("%c\t", *token_start);
-    // printf("%c\t", *(token_start + token_len));
-    // printf("%d\n", token_len);
-
     memset(token.value, '\0', sizeof(token.value));
     strncpy(token.value, token_start, token_len);
     printf("\"%s\"\n", token.value);
 }
+
 
 
 int main(int argc, char *argv[]) {
