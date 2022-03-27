66bc0452685a109feb003ed42dc160a1fa4de552
Bahadır Gezer <bahadirgezerbg@gmail.com>
2022-03-27 14:21:04 +0300

    Tokenizer is working for alphanumerics. Will add all other token types

--- main.c
+++ main.c
@@ -20,45 +20,38 @@ struct Token {
 */
 void tokenizer(char line[]) {
     char *token_start = line;
-    char *token_end = line;
     int token_type = 0; //0: loopbegin; 1: special character; 2: alphanumeric
+    int token_len = 0;
     char current;
 
     for (int i = 0; i < strlen(line); i++) {
         char current = line[i];
-        printf("%c", line[i]);
-        printf("%d\n", token_type);
+        printf("%c\t", line[i]);
+        printf("%d\t", token_type);
+        printf("%d\n", token_len);
 
         if (isWhitespace(current) == 1) {
             if (token_type != 0) {
-                extract_token(token_start, token_end, line);
-            }
-            token_start = line + 1;
-            token_start = line + 1;
-            continue;
-        }
-
-        if (isSpecialCharacter(current) == 1) {
-            if (token_type != 1 || token_type != 0) {
-                extract_token(token_start, token_end, line);
-                token_start = line + i;
-                token_end = line + i;
+                extract_token(token_start, token_len);
+                token_start = token_start + token_len + 1;
                 token_type = 0;
+                token_len = 0;
+            } else {
+                token_start = token_start + 1;
+                token_len = 0;
             }
-            token_type = 1;
-
 
             continue;
         }
 
         if (isAlphaNumeric(current) == 1) {
             if (token_type == 2 || token_type == 0) {    
-                token_end += i;
+                token_len++;
                 token_type = 2;
             } else {
-                extract_token(token_start, token_end, line);
-                token_start = line + i;
-                token_end = line + i;
+                extract_token(token_start, token_len);
+                token_start = token_start + token_len;
+                token_len = 0;
                 token_type = 0;
             }
 
@@ -66,12 +59,18 @@ void tokenizer(char line[]) {
         }
 
     }
+    extract_token(token_start, token_len);
 }
 
-void extract_token(int *token_start, int *token_end, char line[]) {
+void extract_token(int *token_start, int token_len) {
     Token token;
-    int strlen = (*token_start - *token_end) / sizeof(*token_start);
-    strncpy(token_start, token.value, strlen);
+    printf("%p\t", token_start);
+    printf("%c\t", *token_start);
+    printf("%c\t", *(token_start + token_len));
+    printf("%d\n", token_len);
+
+    memset(token.value, '\0', sizeof(token.value));
+    strncpy(token.value, token_start, token_len);
     printf("%s\n", token.value);
 }
 
