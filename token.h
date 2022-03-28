8aa20c63a2577ba0c16491ff01144759e680c647
Bahadır Gezer <bahadirgezerbg@gmail.com>
2022-03-28 08:21:46 +0300

    finished type loopup table

--- token.h
+++ token.h
@@ -10,15 +10,18 @@ typedef struct Token Token;
     0  -> scalar
     1  -> vector
     2  -> matrix
-    3  -> 
-    4  ->
-    5  ->
-    6  ->
-    7  ->
-    8  ->
-    9  ->
-    10 ->
-    11 ->
-    12 ->
-
+    3  -> function keyword (tr, sqrt, choose, print, printsep)
+    4  -> for
+    5  -> left paranthesis
+    6  -> right paranthesis
+    7  -> left square brace
+    8  -> right square brace
+    9  -> left curly brace
+    10 -> right curely brace
+    11 -> equals
+    12 -> multiplication
+    13 -> addition
+    14 -> subtraction
+    15 -> comma 
+    16 -> colon
 */
